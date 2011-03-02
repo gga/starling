1304bf310b9528d20c42380410c37a9412c29ee9
Giles Alexander <giles.alexander@gmail.com>
2011-03-02 12:14:15 +1100

    Don't allow completely duplicate entries.

--- lib/thoughtworker.rb
+++ lib/thoughtworker.rb
@@ -1,3 +1,4 @@
 class ThoughtWorker < ActiveRecord::Base
   validates_presence_of :name, :human_address, :latitude, :longitude, :country
+  validates_uniqueness_of :human_address, :scope => :name
 end
